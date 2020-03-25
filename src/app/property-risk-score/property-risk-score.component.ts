e06c1e71467e447a0f3c9863b1d1d17856d3e510
ANNIJOICE <annijoice96@gmail.com>
2020-03-25 14:56:52 +0530

    CapRate, Cash on cash and 70 ARV rule

--- src/app/property-risk-score/property-risk-score.component.ts
+++ src/app/property-risk-score/property-risk-score.component.ts
@@ -10,6 +10,27 @@ export class PropertyRiskScoreComponent implements OnInit {
   square_feet_width: number;
   square_footage: number;
 
+  rental_income: number;
+  other_income: number;
+  vacancy_loss: number;
+  NOI: number;
+  cap_rate: number;
+  other_expense_loss: number;
+  present_market_value: number;
+
+  purchase_price: number;
+  renovation_price: number;
+  repair_costs: number;
+  arv_value: number;
+  arv_percent_value: number;
+
+  annual_rent: number;
+  mortgage_payment: number;
+  down_payment: number;
+  fees_paid: number;
+  cash_on_cash: number;
+
+
   constructor() { }
 
   ngOnInit(): void {
@@ -19,4 +40,18 @@ export class PropertyRiskScoreComponent implements OnInit {
     this.square_footage = this.square_feet_length * this.square_feet_width;
   }
 
+  calculateCapRate(): void {
+    this.NOI = (+this.rental_income + +this.other_income) - (+this.vacancy_loss + +this.other_expense_loss);
+    this.cap_rate = (this.NOI / this.present_market_value) * 100;
+  }
+
+  calculateARVRate(): void {
+    this.arv_value = +this.purchase_price + +this.renovation_price;
+    this.arv_percent_value = (this.arv_value * 0.70) - this.repair_costs;
+  }
+
+  calculateCashOnCashRate(): void {
+    this.cash_on_cash = (this.annual_rent - this.mortgage_payment) / (+this.down_payment + +this.fees_paid) * 100;
+  }
 }
+
